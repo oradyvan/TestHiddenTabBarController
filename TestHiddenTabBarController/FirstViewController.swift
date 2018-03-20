bf8bcbaaa50abe97c360a2dadc9d443055846b2b
Alexander Lonsky <alonskiy@gmail.com>
2018-03-20 08:45:46 +0100

    Initial Commit

---
+++ TestHiddenTabBarController/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TestHiddenTabBarController
+//
+//  Created by Alexander Lonsky on 20/03/2018.
+//  Copyright © 2018 Alexander Lonsky. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
